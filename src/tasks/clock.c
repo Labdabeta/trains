b8f30e07692f54a455a03d18f46e1a7915da5d4c
tkolomat <tkolomat@student.cs.uwaterloo.ca>
2017-06-03 22:51:49 -0400

    Good night!

--- src/tasks/clock.c
+++ src/tasks/clock.c
@@ -2,35 +2,56 @@
 #include "debugio.h"
 #include "setup.h"
 
-void clock_notifier(){
-	int ticks = 0;
-	int serv_tid = MyParentTid();
-	//Share(serv_tid, &ticks);
+#define PQ_CAPACITY 100
+#include "pq.h"
 
-	while(1){
-		AwaitEvent();
-		DEBUG_PRINT("HWI!");
-		ticks++;
-		Send(serv_tid, 0, 0, 0, 0);
-	}
+void clock_notifier(){
+  int ticks = 0;
+  int serv_tid = MyParentTid();
+  Share(serv_tid, &ticks);
+
+  while(1){
+    AwaitEvent();
+    ticks++;
+    Send(serv_tid, 0, 0, 0, 0);
+  }
 }
 
 void clock_server(){
-	int n_tid = CreateSize(0, clock_notifier, TASK_SIZE_NORMAL);
-	DEBUG_DUMP_VAL(n_tid);
-	int caller;
-	//int *ticks = Obtain(&caller);
-	//Respond(caller, ticks);
-	RegisterAs("CLOCK");
-	//setupTimer();
+  CreateSize(0, clock_notifier, TASK_SIZE_NORMAL);
+  //DEBUG_DUMP_VAL(n_tid);
+  int caller, len, deadline;
+  int *ticks = Obtain(&caller);
+  Respond(caller, ticks);
+  RegisterAs("CLOCK");
 
-	while(1){
-		Receive(&caller, 0, 0);
-		DEBUG_PRINT("Clock server!");
-		Reply(caller, 0, 0);
+  char msg[2];
+  timeheap events;
+  thinit(&events);
+  struct event* top;
 
-		/*debugio_putstr("Time: ");
-		debugio_putint_decimal(*ticks);
-		debugio_putstr("\n\t");*/
-	}
+  while(1){
+    len = Receive(&caller, msg, 2);
+    if(len){
+      if(msg[0] == 't'){
+        Reply(caller, (char*) ticks, sizeof(*ticks));
+      } else{
+        deadline = *ticks;
+        if(msg[0] == 'd')
+          deadline += msg[1];
+        thinsert(&events, deadline, caller);
+      }
+    } else{
+      Reply(caller, 0, 0);
+      while((top = thgetmax(&events))){
+        if(top->time > *ticks)
+          break;
+        Reply(top->tid, 0, 0);
+        thpop(&events);
+      }
+      debugio_putstr("Time: ");
+      debugio_putint_decimal(*ticks);
+      debugio_putstr("\r\n");
+    }
+  }
 }
