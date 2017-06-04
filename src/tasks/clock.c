6b7611911b00e76a6f79a20b256b9ff7dd770fbc
tkolomat <tkolomat@nettop1.student.cs.uwaterloo.ca>
2017-06-04 13:12:05 -0400

    periodic events

--- src/tasks/clock.c
+++ src/tasks/clock.c
@@ -19,26 +19,24 @@ void clock_notifier(){
 
 void clock_server(){
   CreateSize(0, clock_notifier, TASK_SIZE_NORMAL);
-  //DEBUG_DUMP_VAL(n_tid);
   int caller, len, deadline;
   int *ticks = Obtain(&caller);
   Respond(caller, ticks);
   RegisterAs("CLOCK");
-
-  char msg[2];
   timeheap events;
   thinit(&events);
   struct event* top;
+  char msg[2];
 
   while(1){
     len = Receive(&caller, msg, 2);
     if(len){
-      if(msg[0] == 't'){
+      if(len == 1){
         Reply(caller, (char*) ticks, sizeof(*ticks));
       } else{
-        deadline = *ticks;
+        deadline = msg[0];
         if(msg[0] == 'd')
-          deadline += msg[1];
+          deadline += *ticks;
         thinsert(&events, deadline, caller);
       }
     } else{
