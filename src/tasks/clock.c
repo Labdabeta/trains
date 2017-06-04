2ad8de72b6f4a3d506aca5dbd634e3c0eee256ed
tkolomat <tkolomat@nettop1.student.cs.uwaterloo.ca>
2017-06-04 18:46:57 -0400

    fixes are niceness

--- src/tasks/clock.c
+++ src/tasks/clock.c
@@ -6,52 +6,44 @@
 #include "pq.h"
 
 void clock_notifier(){
-  int ticks = 0;
   int serv_tid = MyParentTid();
-  Share(serv_tid, &ticks);
-
   while(1){
     AwaitEvent();
-    ticks++;
     Send(serv_tid, 0, 0, 0, 0);
   }
 }
 
 void clock_server(){
   CreateSize(0, clock_notifier, TASK_SIZE_NORMAL);
-  //DEBUG_DUMP_VAL(n_tid);
   int caller, len, deadline;
-  int *ticks = Obtain(&caller);
-  Respond(caller, ticks);
+  int ticks = 0;
   RegisterAs("CLOCK");
 
-  char msg[2];
+  struct intandflag msg;
   timeheap events;
   thinit(&events);
   struct event* top;
 
   while(1){
-    len = Receive(&caller, msg, 2);
+    len = Receive(&caller, (char *)&msg, sizeof(struct intandflag));
     if(len){
-      if(msg[0] == 't'){
-        Reply(caller, (char*) ticks, sizeof(*ticks));
+      if(len == 1){
+        Reply(caller, (char*) ticks, sizeof(ticks));
       } else{
-        deadline = *ticks;
-        if(msg[0] == 'd')
-          deadline += msg[1];
+        deadline = msg.val;
+        if(msg.code == 'd')
+          deadline += ticks;
         thinsert(&events, deadline, caller);
       }
     } else{
       Reply(caller, 0, 0);
+      ticks++;
       while((top = thgetmax(&events))){
-        if(top->time > *ticks)
+        if(top->time > ticks)
           break;
         Reply(top->tid, 0, 0);
         thpop(&events);
       }
-      debugio_putstr("Time: ");
-      debugio_putint_decimal(*ticks);
-      debugio_putstr("\r\n");
     }
   }
 }
