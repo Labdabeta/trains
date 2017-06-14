aed76ca16165f3536a32ad38b4b947798a230634
tkolomat <tkolomat@nettop1.student.cs.uwaterloo.ca>
2017-06-13 21:44:06 -0400

    COM1 interactions done

--- src/tasks/marklin_view.c
+++ src/tasks/marklin_view.c
@@ -1,15 +1,37 @@
 #include "model.h"
+#include "marklin_view.h"
 #include "tasks.h"
 
-static struct reverse_coordinator_args{
+struct reverse_coordinator_args{
 	int train;
 	int speed;
 	int out_tid;
 	int clock_tid;
+};
+
+void reverse_coordinator(){
+	int caller;
+	struct reverse_coordinator_args rev_args;
+
+	Receive(&caller, (char *) &rev_args, sizeof(struct reverse_coordinator_args));
+	Reply(caller, 0, 0);
+
+	char msg[3];
+	msg[0] = rev_args.speed < 16 ? 0 : 16;
+	msg[1] = rev_args.train;
+	msg[2] = 0;
+	Putstr(rev_args.out_tid, 1, msg);
+	Delay(rev_args.clock_tid, 300);
+	msg[0] = 15;
+	Putstr(rev_args.out_tid, 1, msg);
+	Delay(rev_args.clock_tid, 5);
+	msg[0] = rev_args.speed;
+	Putstr(rev_args.out_tid, 1, msg);
+	Exit();
 }
 
 void train_state_view(){
-	int caller;
+	int caller, child_tid;
 	struct Train_Command request;
 
 	struct reverse_coordinator_args rev_args;
@@ -25,56 +47,54 @@ void train_state_view(){
 
 	int trainstates[81];
 	for(msg[1] = 1; msg[1] < 81; msg[1]++){
-		trainstates[msg[1]] = 0;
+		trainstates[(int) msg[1]] = 0; // NB check
 		Putstr(out_tid, 1, msg);
 	}
 
-	Recieve(&caller, 0, 0);
+	Receive(&caller, 0, 0);
 	Reply(caller, 0, 0);
 
 	while(1){
-		Recieve(&caller, (char *) &request, sizeof(struct Train_Command));
+		Receive(&caller, (char *) &request, sizeof(struct Train_Command));
+		Reply(caller, 0, 0);
 		switch(request.type){
 			case TYPE_SetSpeed:
-				trainstates[msg.command.speed.train] = msg.command.speed.speed] + (trainstates[msg.command.speed.train] & 16);
-				msg[0] = trainstates[msg.command.speed.train];
-				msg[1] = msg.command.speed.train;
+				trainstates[request.args.speed.train] = request.args.speed.speed + (trainstates[request.args.speed.train] & 16);
+				msg[0] = trainstates[request.args.speed.train];
+				msg[1] = request.args.speed.train;
 				Putstr(out_tid, 1, msg);
 			break;
 
 			case TYPE_Reverse:
 				child_tid = CreateSize(0, reverse_coordinator, TASK_SIZE_TINY);
-				rev_args.train = msg.command.reverse.train;
-				rev_args.speed = trainstates[msg.command.reverse.train];
+				rev_args.train = request.args.reverse.train;
+				rev_args.speed = trainstates[request.args.reverse.train];
 				Send(child_tid, (char *) &rev_args, sizeof(struct reverse_coordinator_args), 0, 0);
 			break;
 
 			case TYPE_Headlights:
-				trainstates[msg.command.lights.train] ^= 16;
-				msg[0] = trainstates[msg.command.lights.train];
-				msg[1] = msg.command.lights.train;
+				trainstates[request.args.lights.train] ^= 16;
+				msg[0] = trainstates[request.args.lights.train];
+				msg[1] = request.args.lights.train;
 				Putstr(out_tid, 1, msg);
 			break;
 		}
-		Reply(caller, 0, 0);
 	}
 }
 
-void reverse_coordinator(){
+void switch_coordinator(){
 	int caller;
-	struct reverse_coordinator_args rev_args;
+	struct switch_coordinator_args flip_args;
 
-	Recieve(&caller, (char *) &rev_args, sizeof(struct reverse_coordinator_args));
+	Receive(&caller, (char *) &flip_args, sizeof(struct switch_coordinator_args));
 	Reply(caller, 0, 0);
 
 	char msg[3];
-	msg[0] = rev_args.speed < 16 ? 0 : 16;
-	msg[1] = rev_args.train;
-	msg[3] = 0;
-	Putstr(rev_args.out_tid, 1, msg);
-	Delay(rev_args.clock_tid, 300);
-	Putc(rev_args.out_tid, 1, 15);
-	Delay(rev_args.clock_tid, 5);
-	Putc(rev_args.out_tid, 1, rev_args.speed);
+	msg[0] = flip_args.state;
+	msg[1] = flip_args.number;
+	msg[2] = 0;
+	Putstr(flip_args.out_tid, 1, msg);
+	Delay(flip_args.clock_tid, 5);
+	Putc(flip_args.out_tid, 1, 32);
 	Exit();
 }
